ceb644e8b13f81eb92ab2fcece0697656fdbf542
kianabel <kian.leba@gmail.com>
2023-11-06 18:06:14 +0100

    waller

--- grungus/src/index.js
+++ grungus/src/index.js
@@ -28,7 +28,7 @@ const createWindow = () => {
   
 
   // Open the DevTools.
- // mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools();
 
  globalShortcut.register('CommandOrControl+R', () => {
   mainWindow.reload();
